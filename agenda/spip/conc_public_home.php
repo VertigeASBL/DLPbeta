457c8c18663dc8604ab98a6828372abdb9d177f2
Phil <philippe@vertige.org>
2012-12-20 16:59:28 +0100

    deplacer fonctions de concours /home

--- agenda/spip/conc_public_home.php
+++ agenda/spip/conc_public_home.php
@@ -3,6 +3,83 @@ require 'agenda/inc_var.php';
 require 'agenda/inc_db_connect.php';
 include_spip('inc/utils');
 
+	/*** Redimensionner les photos à 100px de larg ***/
+	$larg_max = 100;
+	function vignette_home($image,$w_vi_absolue,$nom){
+		// Largeur et hauteur initiales
+		$uploaded_pic = imagecreatefromjpeg($image); // = photo uploadée 
+		$largeur_uploaded = imagesx($uploaded_pic);
+		$hauteur_uploaded = imagesy($uploaded_pic);	
+	
+		if ($largeur_uploaded<=$w_vi_absolue)
+		{	
+			$new_W_Vignette = $largeur_uploaded ;
+			$new_H_Vignette = $hauteur_uploaded ;
+		}
+		else
+		{		
+			// W > maximum
+			if ($largeur_uploaded>$w_vi_absolue)
+			{
+					$new_W_Vignette = $w_vi_absolue;
+					// On recalcule la Hauteur proportionnellement
+					$new_H_Vignette = round($hauteur_uploaded * $w_vi_absolue / $largeur_uploaded);
+			}
+		}
+				
+		$resample = imagecreatetruecolor($new_W_Vignette, $new_H_Vignette); // Création image vide
+		imagecopyresampled($resample, $uploaded_pic, 0, 0, 0, 0, $new_W_Vignette, $new_H_Vignette, $largeur_uploaded, $hauteur_uploaded);
+		$destination_vi = 'agenda/vignettes_home/'.$nom.'.jpg';
+		@unlink($destination_vi);
+		imagejpeg($resample, $destination_vi);
+		
+		//Renvoie la hauteur de la vignette		
+		return $new_H_Vignette;
+	}	
+
+	/**** Couper le texte pour que la hauteur s'adapte à la hauteur de l'image ***/
+	function nb_cars($haut_img){
+		$hors_txt = 65;		//nb px hors texte à adapter
+		$px_ligne = 15;		//nb px par ligne
+		$car_ligne = 50;	//nb car par ligne
+		
+		if ($haut_img < $hors_txt){	//1 ligne min
+			$nb_car = $car_ligne;
+		}else{
+			//Nb de lignes à afficher
+			$nb_ligne = floor(($haut_img - $hors_txt) / $px_ligne);
+			//Nb de caractères à garder
+			if ($nb_ligne < 1) 
+				$nb_car = $car_ligne;
+			else
+				$nb_car = $nb_ligne * $car_ligne;
+		}
+		return $nb_car;
+	}	
+
+	function couper_txt($texte,$nb_car){
+		//Texte découpé à afficher
+		$order   = array("\r\n", "\n", "\r");
+		$texte = str_replace($order, '', $texte);
+		
+		//remplacer les <br> avec espace
+		$br   = array("<br />", "<BR />", "<BR >","<br >");
+		$texte = str_replace($br, '<-xx->', $texte);
+		
+		//texte raccourci
+		$texte = substr ($texte, 0, $nb_car);		
+		
+		$a_couper = strlen(strrchr($texte, " "));
+		$texte = substr($texte, 0, strlen($texte)-$a_couper);	//tronquer au dernier mot
+		$texte = $texte.' ...';
+			
+		//remettre les br
+		$texte = str_replace('<-xx->', '<br />', $texte);
+		$texte = str_replace("<br /><br />", "<br />'", $texte);
+		
+		return $texte; 
+	}	
+
 	//---------------------------------------------------------------------------
 	// Listing des concours (n'est pas affiché quand le visiteur joue pour un élément sélectionné
 	//---------------------------------------------------------------------------
